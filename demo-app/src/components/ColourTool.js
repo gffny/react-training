f094e3de798fc050cb8946cc34b9b035425f5f67
John Gaffney <john.gaffney@plastiq.com>
2019-06-10 14:05:14 -0400

    Added form to component

--- demo-app/src/components/ColourTool.js
+++ demo-app/src/components/ColourTool.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const ColourTool = ({ colourList }) => {
+
+    // useState returns an array, the first value of which returns the value, the second value is a function to update the value
+    const [newColourInput, setNewColourInput] = useState('');
 
     const listItems = (itemList, itemFormatterFn) => itemList.map((item, index) =>
         <li key={index}>{itemFormatterFn(item)}</li>
     );
+
+    /*  without this function the real DOM cannot update the virtual DOM. 
+        The virtual DOM cannot be updated by the real DOM, 
+        it can trigger a re-rendering, which is what this function does */
+    const change = (e) => setNewColourInput(e.target.value);
 
     return <>
         <header>
@@ -13,5 +21,11 @@ export const ColourTool = ({ colourList }) => {
         <ul>
             {listItems(colourList, item => item.toUpperCase())}
         </ul>
+        <form>
+            <div>
+                <label>New Colour </label>
+                <input type="text" id="new-colour-input" value={newColourInput} onChange={change} />
+            </div>
+        </form>
     </>;
 };
